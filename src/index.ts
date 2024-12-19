8a4d9410eba5feeceaadb726471590dea00ef1a5
Andres Mahor <andresmahor@gmail.com>
2024-12-18 21:18:46 -0500

    Add workflow deploy

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Cloudflare Workers! Bun");
+  return c.text("Hello Cloudflare Workers! Bun JS");
 });
 
 export default app;
